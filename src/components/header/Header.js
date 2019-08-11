a01bc02a2f0f3a3e88f2fad14fc19bcd3c578d08
durgakiran <gatemefuture@gmail.com>
2019-08-11 09:30:16 +0530

    meme generator capston project
    everythng implemented in react no node involved

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface-PNG-Image.png" 
+            alt="problem?"/>
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header;
